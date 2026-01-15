ea4829d609f2ad24279d83a20472a1ca0f0b14e4
ismaya nasuha <ismayanasuha@MacBook-Pro-ismaya.local>
2026-01-16 01:29:33 +0700

    last commit

---
+++ dist/_astro/index-client.0kLl3mW7.js
@@ -0,0 +1,1 @@
+import{a5 as o,a6 as t,a3 as c}from"./template.5Sk2R7wT.js";function f(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function u(e){o===null&&f(),t(()=>{const n=c(e);if(typeof n=="function")return n})}export{u as o};
